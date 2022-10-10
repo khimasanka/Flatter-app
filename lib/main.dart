d7088a355df93f79051b7a90290670f497906c21
Kaveesha Himasanka <kaveee143@gmail.com>
2022-10-10 22:11:31 +0530

    matrix4 rotation

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -47,7 +48,6 @@ class MyHomePage extends StatelessWidget {
             tabs: [
               Tab(
                 icon: Icon(Icons.directions_car),
-                text: 'Car',
               ),
               Tab(
                 icon: Icon(Icons.directions_transit),
@@ -55,15 +55,33 @@ class MyHomePage extends StatelessWidget {
               Tab(icon: Icon(Icons.directions_bike)),
             ],
           ),
+          elevation: 22.0,
+          backgroundColor: Colors.black87,
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: [
-            Icon(Icons.ice_skating),
-            Icon(Icons.adb_sharp),
-            Icon(Icons.media_bluetooth_off_sharp),
+            tab1(),
+            const Icon(Icons.adb_sharp),
+            const Icon(Icons.media_bluetooth_off_sharp),
           ],
         ),
       ),
     );
   }
+}
+
+Widget tab1() {
+  return Container(
+    padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+    width: double.infinity,
+    color: Colors.red,
+    transform: Matrix4.rotationZ(0.1),
+    child: const Text(
+      'Car Widget Text',
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
 }
